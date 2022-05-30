cc66014f220e423ea39fae150eb3a10501600594
lukendev <lucas.azevedo2104@gmail.com>
2022-05-30 11:15:58 -0300

    Comentarios

---
+++ exercicios/src/fundamentos/Comentarios.java
@@ -0,0 +1,37 @@
+package fundamentos;
+
+	/**
+	 * Essa classe serve para....
+	 * 
+	 * @author lukendev <lucas.azevedo2104@gmail.com>
+	 * @since JDK13.0.2
+	 */
+
+public class Comentarios {
+	
+	/**
+	 * ...
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ;
+		// Mais um comentário...
+		// Fim
+		
+		/*
+		 
+		  Linha 1
+		  Linha 2
+		  Linha 3...
+		  
+		 */
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("Fim"); // Aqui também funciona!
+		
+	}
+
+}

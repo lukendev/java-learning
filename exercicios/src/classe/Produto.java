4d49f6b830491b6aa5801cadc856bbb76c189ca0
lukendev <lucas.azevedo2104@gmail.com>
2022-06-03 11:29:11 -0300

    Construtores

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -6,10 +6,21 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	
+	Produto(){
+		
+	}
+	
+	Produto(String nomeInicial, double precoInicial, double descontoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoInicial;
+	}
+	
+	
 	double precoComDesconto(){
 		return preco * (1-desconto);
 	}
-	
 	double precoComDesconto(double descontoDoGerente){
 		return preco * (1-desconto+descontoDoGerente);
 	}

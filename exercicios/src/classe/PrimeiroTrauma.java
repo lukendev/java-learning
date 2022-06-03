cc710bf586457f20d4c95ece7d4c7bb82dbebb47
lukendev <lucas.azevedo2104@gmail.com>
2022-06-03 15:21:57 -0300

    Desafio primeiro trauma

---
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+	int a = 3; // não pode mexer aqui!
+	static int b = 4;
+
+	public static void main(String[] args) {
+
+		PrimeiroTrauma p = new PrimeiroTrauma();
+		System.out.println(p.a);
+		System.out.println(b);
+	}
+}

25b79bcf4567c6235764c4c2246f3e47e68bd46b
lukendev <lucas.azevedo2104@gmail.com>
2022-06-03 15:55:53 -0300

    This e this()

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -6,15 +6,10 @@ public class DataTeste {
 		
 		
 		Data d1 = new Data();
+		d1.ano = 2021;
 		Data d2 = new Data(31, 12, 2020);
 		
-		
-//		d1.dia = 31;
-//		d1.mes = 12;
-//		d1.ano = 2020;
-		
 		String dataFormatada1 = d1.obterDataFormatada();
-		
 		
 		System.out.println(dataFormatada1);
 		System.out.println(d2.obterDataFormatada());

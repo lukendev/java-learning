20cbc5056b6f0f820ce95b10804efa07a12de294
lukendev <lucas.azevedo2104@gmail.com>
2022-06-08 10:10:49 -0300

    Getters and Setters

---
+++ exercicios/src/oo/encapsulamento/Pessoa.java
@@ -0,0 +1,56 @@
+package oo.encapsulamento;
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	private String sobrenome;
+	
+	public Pessoa(String nome, String sobrenome, int idade) {
+		setNome(nome);
+		setSobrenome(sobrenome);
+		setIdade(idade);
+	}
+	
+	
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNomeCompleto() {
+		return getNome() + " " + getSobrenome();
+	}
+
+
+	// getter
+	public int getIdade() {
+		return idade;
+	}
+	
+	// setter
+	public void setIdade(int novaIdade) {
+		novaIdade = Math.abs(novaIdade);
+		if(novaIdade >= 0 && novaIdade <= 120) {
+			this.idade = novaIdade;			
+		}
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Olá eu sou o " + getNome() + " e tenho " + getIdade() + " anos.";
+	}
+}
---
+++ exercicios/src/oo/encapsulamento/PessoaTeste.java
@@ -0,0 +1,14 @@
+package oo.encapsulamento;
+
+public class PessoaTeste {
+
+	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa("Pedro", "Amoedo", 45);
+//		p1.idade = -30; // alterar
+		p1.setIdade(230);
+		
+		System.out.println(p1.getIdade()); // ler
+		System.out.println(p1); // ler
+		System.out.println(p1.getNomeCompleto());
+	}
+}

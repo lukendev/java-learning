538f08a98c5968cd3891f0cd35736d36b1ac5d79
lukendev <lucas.azevedo2104@gmail.com>
2022-06-08 09:57:16 -0300

    Desafio modificadores de acesso

--- exercicios/src/oo/heranca/desafio/Carro.java
+++ exercicios/src/oo/heranca/desafio/Carro.java
@@ -2,22 +2,22 @@ package oo.heranca.desafio;
 
 public class Carro {
 
-	final int VELOCIDADE_MAXIMA;
-	int velocidadeAtual;
-	int delta = 5;
+	public final int VELOCIDADE_MAXIMA;
+	protected int velocidadeAtual;
+	protected int delta = 5;
 	
-	Carro(int velocidadeMaxima){
+	protected Carro(int velocidadeMaxima){
 		VELOCIDADE_MAXIMA = velocidadeMaxima;
 	}
 	
-	void acelerar() {
+	public void acelerar() {
 		if(velocidadeAtual + delta > VELOCIDADE_MAXIMA) {
 			velocidadeAtual = VELOCIDADE_MAXIMA;
 		}
 		velocidadeAtual += delta;
 	}
 	
-	void frear() {
+	public void frear() {
 		if(velocidadeAtual >= 5) {
 			velocidadeAtual -= 5;			
 		} else {
